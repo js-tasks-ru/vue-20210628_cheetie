4accc7ba802ec37341da7451e932c8abd1526195
Nomta <nomta@mail.ru>
2021-11-10 08:50:11 +0300

    meetup description props type

--- 02-components/02-MeetupDescription/MeetupDescription.js
+++ 02-components/02-MeetupDescription/MeetupDescription.js
@@ -2,7 +2,12 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupDescription',
-  props: ['description'],
+
+  props: {
+    description: {
+      type: String
+    }
+  },
 
   template: `<p class="meetup-description">{{description}}</p>`,
 });
