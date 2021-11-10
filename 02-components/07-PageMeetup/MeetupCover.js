4e1750de2c1f9e497edd2178507b912392516ce9
Nomta <nomta@mail.ru>
2021-11-10 22:50:59 +0300

    page meetup

---
+++ 02-components/07-PageMeetup/MeetupCover.js
@@ -0,0 +1,17 @@
+import {
+  defineComponent
+} from './vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'MeetupCover',
+  props: ['title', 'image'],
+  computed: {
+    style() {
+      return this.image && `--bg-url: url(${this.image})`
+    }
+  },
+  template: `
+    <div class="meetup-cover" :style="style">
+        <h1 class="meetup-cover__title">{{title}}</h1>
+    </div>`,
+});
--- 02-components/07-PageMeetup/PageMeetup.js
+++ 02-components/07-PageMeetup/PageMeetup.js
@@ -1,26 +1,66 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
+import MeetupView from './MeetupView.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import { fetchMeetupById } from './meetupService.js';
 
 export default defineComponent({
   name: 'PageMeetup',
 
   components: {
+    MeetupView,
     UiAlert,
     UiContainer,
+  },
+  
+  props: {
+    meetupId: {
+      type: Number,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      meetup: null,
+      error: null,
+      loading: false
+    }
+  }, 
+  
+  methods: {
+    loadMeetup() {
+      this.meetup = null;
+      this.error = null;
+      this.loading = true;
+      
+      fetchMeetupById(this.meetupId)
+        .then(meetup => this.meetup = meetup)
+        .catch(error => this.error = error)
+        .then(() => this.loading = false);
+    }
+  },
+  
+  mounted() {
+    this.loadMeetup();
+  },
+  
+  watch: {
+    meetupId() {
+      this.loadMeetup();
+    }
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <meetup-view v-if="meetup" :meetup="meetup"/>
 
-      <ui-container>
+      <ui-container v-else-if="loading">
         <ui-alert>Загрузка...</ui-alert>
       </ui-container>
 
-      <ui-container>
-        <ui-alert>error</ui-alert>
+      <ui-container v-else-if="error">
+        <ui-alert>{{error.message}}</ui-alert>
       </ui-container>
     </div>`,
 });
