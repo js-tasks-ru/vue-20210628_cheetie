245929819737694ce75eb8004d41fe01d4073951
Nomta <nomta@mail.ru>
2021-11-10 08:46:44 +0300

    counter button props type

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -2,7 +2,12 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'CounterButton',
-  props: ['count'],
+  props: {
+    'count': {
+      type: Number,
+      default: 0
+    }
+  },
   methods: {
     updateCount() {
       this.$emit('update:count', this.count + 1)
